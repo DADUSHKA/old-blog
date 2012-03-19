3f44d58163e1318f70cb116ba073ad343338aa6e
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2012-03-19 19:37:16 +0000

    123

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
 
   private
   def parse_hash_tags
-    tag_list.clear
-    tag_list = extract_hashtags(body)
+    self.tag_list.clear
+    self.tag_list = extract_hashtags(body)
   end
 end
