be6685c76d479eed8b41b95eec8a257e274d027a
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2012-03-24 17:09:54 +0000

    bradcrumbs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,12 @@
-class PostsController < InheritedResources::Base
-  actions :index, :show
+class PostsController < ApplicationController
+  add_breadcrumb I18n.t('posts'), :posts_path
+  
+  def index 
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find params[:id]
+    add_breadcrumb @post.title, post_path(@post)
+  end
 end
