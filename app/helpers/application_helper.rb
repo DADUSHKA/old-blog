7a01236900f7e39e01cf901ffcc16211243f2bb3
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2012-04-07 20:41:57 +0000

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def display_tree (roots, options = {}, &block)
     return '' if roots.empty?
     items = roots.inject('') do |string, node|
-      item = yield(node) + display_tree_recursive(node.children, options, &block)
+      item = yield(node) + display_tree(node.children, options, &block)
       string + content_tag(:li, item, options[:li], false)
     end
     content_tag :ul, items, options[:ul], false
